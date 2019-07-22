4d3f9536f405288441ec22bcbc1562867cf1c662
Dylan Landry <dylanglandry@gmail.com>
2019-07-22 10:08:04 -0400

    Can use up/down to navigate transaction list.

--- src/components/TransactionList.tsx
+++ src/components/TransactionList.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import styles from './TransactionList.module.css'
 import { TransactionsState } from '../store/transaction/types';
 import Transaction from './Transaction'
 import TransactionDetailView from './TransactionDetail';
 import TFilter from './TFilter';
 import { DetailedTransaction } from '../store/detailedTransaction/types';
+
+let keydownEventListener: any = null
 
 function TransactionList({
   transactions = {},
@@ -22,6 +24,37 @@ function TransactionList({
     ...t,
   }))
   tArray.sort((t1, t2) => t1.date - t2.date)
+
+  const tListRef = useRef<HTMLOListElement>(null)
+  // I do not think these keyboard interactions are even required of aria w3 spec.
+  // It's just a list with popups, it isn't a menu.
+  if (tListRef.current !== null && keydownEventListener === null) {
+    // cache event listener
+    keydownEventListener = tListRef.current.addEventListener('keydown', (event: KeyboardEvent) => {
+      // Would imply the transaction list doesn't exist
+      if (tListRef.current === null) return
+      // Listener implies a child must be focused
+      if (document.activeElement === null) return
+
+      const activeElement = document.activeElement as HTMLElement
+      const childButtons = tListRef.current.querySelectorAll<HTMLElement>(`button.${styles.transactionButton}`)      
+      const focusedButtonIndex = Array.from(childButtons).indexOf(activeElement)
+      switch (event.key) {
+        case 'ArrowDown':
+        case 'Down':
+          childButtons[(focusedButtonIndex + 1) % childButtons.length].focus()
+          break
+        case 'ArrowUp':
+        case 'Up':
+            childButtons[focusedButtonIndex === 0
+              ? childButtons.length - 1
+              : focusedButtonIndex - 1
+            ].focus()
+          break
+      }
+   })
+  }
+
   return (
     <div className={styles.container}>
       <h2 className={styles.transactionsHeader}>
@@ -34,6 +67,7 @@ function TransactionList({
           className={styles.list}
           role='region'
           aria-label='transactions list'
+          ref={tListRef}
         >
           {tArray.length === 0 &&
             <li className={styles.noTransactions}>No transactions found.</li>
--- todo.md
+++ todo.md
@@ -1,11 +1,5 @@
 # To do
-- [ ] Aria transactions
 - [ ] make balance do some cool animation
-- [ ] figure out how to make transactions aria accessible
 - [ ] maybe indicate that you can scroll down in transaction detail view?
 - [ ] Pop up saying you cannot approve because you would overdraft.
 - [ ] Approve popup?
-
-## Aria Transaction Notes
-- Need to add deposit / withdrawal information for screen readers. -/+ isn't enough.
-- transaction date aria label "received january 25th, 2019 at 11:59 am"
